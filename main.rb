e53d9930a463d1482f080ae57ae60ac5ac28032a
23khushi <solankikhushi951@gmail.com>
2026-03-23 18:16:57 +0530

    Perform Delete Operation and made methods private

--- main.rb
+++ main.rb
@@ -1,41 +1,46 @@
 require_relative "./Bank.rb"
 require_relative "./Account.rb"
 
+class CustomException < StandardError
+    def initialize(message = "Invalid Input")
+        super(message)
+    end
+end
 
   users = [
-    {adhar_no: 985455555555,
-    mobile_no: 7685940876,
+    {adhar_no: "985455555555",
+    mobile_no: "7685940876",
     name: "Khushi Solanki", 
-    acc_type:"Saving",
-    acc_no: 0, 
-    balance:1000,
-    pass: 2222
+    acc_type: "Saving",
+    acc_no: "0", 
+    balance:500,
+    pass: "2222"
 },
-  { adhar_no: 010101010101,
-    mobile_no: 9869557604,
-    name: "ChitraBhide", 
-    acc_type:"Current",
-    acc_no: 0, 
+  { adhar_no: "343434343434",
+    mobile_no: "9869557604",
+    name: "Chitra Bhide", 
+    acc_type: "Current",
+    acc_no: "0", 
     balance:2000,
-    pass: 3333 
+    pass: "3333" 
   },
 
-  { adhar_no: 673289013456,
-    mobile_no: 8739001245,
+  { adhar_no: "673289013456",
+    mobile_no: "8739001245",
     name: "Deshna Patwa", 
-    acc_type:"Saving",
-    acc_no: 0, 
+    acc_type: "Saving",
+    acc_no: "0", 
     balance:1500,
-    pass: 2323 
+    pass: "2323" 
   },
 
-  { adhar_no: 233445456576,
-    mobile_no: 7685940876,
+  { adhar_no: "233445456576",
+    mobile_no: "7685940876",
     name: "Khushi Solanki", 
-    acc_type:"Current",
-    acc_no: 0, 
+    acc_type: "Current",
+    acc_no: "0", 
     balance:3000,
-    pass: 1111
+    pass: "1111"
   }
 
   ]
@@ -52,8 +57,10 @@ while (true)
            \n2)Deposit Money 
            \n3)Withdraw Money
            \n4)Check Balance
-           \n5)Exit"
-    
+           \n5)Update account
+           \n6)Delete account
+           \n7)Exit"
+
         input = Integer(gets.chomp)
         case input
         when 1
@@ -61,20 +68,23 @@ while (true)
                 account.create_account(user)
             end
         when 2
-            account.depositMethod 
+            account.deposit()
         when 3 
-            account.withdrawMethod
+            account.withdraw()
         when 4
-             account.showBalance
+             account.show_balance()
         when 5
+            puts "Feature incoming"
+        when 6
+            account.delete_account()
+        when 7
             puts "Thank You !!! Exiting from the System."
             break
         else 
-            raise "Invalid Option"
-
+            raise ("Invalid Input")
         end
-    rescue => e
-        puts "Error: #{e}"
+    rescue ArgumentError
+        puts "Error: Please Enter valid input"
         retry
     end
 end
